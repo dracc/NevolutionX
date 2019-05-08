ee895bf1220ae3309a7e1e1805cbffe9639e321d
Lucas Eriksson <dracc@dracc.se>
2019-05-08 11:08:21 +0200

    input: Use SDL GameController API

--- Includes/subsystems.cpp
+++ Includes/subsystems.cpp
@@ -7,13 +7,12 @@
 int init_systems() {
 #ifdef NXDK
   XVideoSetMode(640,480,32,REFRESH_DEFAULT);
-  XInput_Init();
   if (pb_init() != 0) {
     return 3;
   }
   pb_show_debug_screen();
 #endif
-  if (SDL_VideoInit(NULL) != 0) {
+  if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_GAMECONTROLLER) != 0) {
     outputLine("Init error: %s", SDL_GetError());
     return 2;
   }
@@ -33,7 +32,6 @@ void shutdown_systems(int systems) {
     SDL_Quit();
   }
 #ifdef NXDK
-  XInput_Quit();
   if (systems <= 2) {
     pb_kill();
   }
