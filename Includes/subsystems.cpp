398c00b2a6ae4b74e7612cf1155c929ad468bde1
Stefan Schmidt <thrimbor.github@gmail.com>
2020-01-04 10:54:48 +0100

    init: Mount drives

--- Includes/subsystems.cpp
+++ Includes/subsystems.cpp
@@ -1,6 +1,7 @@
 #include "subsystems.h"
 
 #ifdef NXDK
+#include <nxdk/mount.h>
 #include <hal/video.h>
 #include <winapi/synchapi.h>
 #endif
@@ -11,6 +12,28 @@ int init_systems() {
   void *p = NULL;
   while (XVideoListModes(&xmode, 0, 0, &p)) {}
   XVideoSetMode(xmode.width, xmode.height, xmode.bpp, xmode.refresh);
+
+  if (!nxMountDrive('C', "\\Device\\Harddisk0\\Partition2")) {
+    outputLine("Mounting error: Could not mount drive C\n");
+  }
+  if (!nxMountDrive('E', "\\Device\\Harddisk0\\Partition1")) {
+    outputLine("Mounting error: Could not mount drive E\n");
+  }
+  if (!nxMountDrive('F', "\\Device\\Harddisk0\\Partition6")) {
+    outputLine("Mounting error: Could not mount drive F\n");
+  }
+  if (!nxMountDrive('G', "\\Device\\Harddisk0\\Partition7")) {
+    outputLine("Mounting error: Could not mount drive G\n");
+  }
+  if (!nxMountDrive('X', "\\Device\\Harddisk0\\Partition3")) {
+    outputLine("Mounting error: Could not mount drive X\n");
+  }
+  if (!nxMountDrive('Y', "\\Device\\Harddisk0\\Partition4")) {
+    outputLine("Mounting error: Could not mount drive Y\n");
+  }
+  if (!nxMountDrive('Z', "\\Device\\Harddisk0\\Partition5")) {
+    outputLine("Mounting error: Could not mount drive Z\n");
+  }
 #endif
   if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_GAMECONTROLLER) != 0) {
     outputLine("Init error: %s", SDL_GetError());
