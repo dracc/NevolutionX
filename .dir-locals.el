3560bf15197a2f3a74e8710135df15e96efc567c
Lucas Eriksson <dracc@dracc.se>
2020-01-19 20:39:32 +0100

    meta: Add c++-mode to emacs style config

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,8 @@
 	    (c-default-style . "stroustrup")
 	    (c-basic-offset . 2))
 	 )
+ (c++-mode . ((indent-tabs-mode . nil)
+	      (c-default-style . "stroustrup")
+	      (c-basic-offset . 2))
+	   )
 )
