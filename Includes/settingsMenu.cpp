c2303868fca49ec31d6a2213c0868154546e813f
Lucas Eriksson <dracc@dracc.se>
2021-03-19 15:53:21 +0100

    settings: Add cache wiping option

--- Includes/settingsMenu.cpp
+++ Includes/settingsMenu.cpp
@@ -1,6 +1,7 @@
 #include "settingsMenu.hpp"
 #include "langMenu.hpp"
 #include "timeMenu.hpp"
+#include "wipeCache.hpp"
 
 settingsMenu::settingsMenu(MenuNode *parent, std::string const& label) :
 MenuNode(parent, label) {
@@ -10,5 +11,8 @@ MenuNode(parent, label) {
 void settingsMenu::init() {
   addNode(std::make_shared<LangMenu>(this, "Language select"));
   addNode(std::make_shared<TimeMenu>(this, "Timezone select"));
+  addNode(std::make_shared<MenuExec>("Wipe cache partitions", [](Menu *){
+    wipe_cache();
+  }));
 }
 
---
+++ Includes/wipeCache.hpp
@@ -0,0 +1,6 @@
+#ifndef __WIPE_CACHE
+#define __WIPE_CACHE
+
+bool wipe_cache();
+
+#endif
