e874e37be3ba2bb45343c07a4db1705e7bd06119
Lucas Eriksson <dracc@dracc.se>
2021-04-26 16:09:49 +0200

    settings: Add EEPROM query template, Audio and Video menus

---
+++ Includes/eeprom.hpp
@@ -0,0 +1,21 @@
+#ifndef __EEPROM_H
+#define __EEPROM_H
+
+#include <cstdint>
+#include <xboxkrnl/xboxkrnl.h>
+
+template<typename T>
+T getEEPROMValue(uint32_t valueIndex) {
+  ULONG Type = 0;
+  return getEEPROMValue<T>(valueIndex, Type);
+}
+
+template<typename T>
+T getEEPROMValue(uint32_t valueIndex, ULONG &Type) {
+  T value;
+  ULONG ResultLength = 0;
+  ExQueryNonVolatileSetting(valueIndex, &Type, &value, sizeof(value), &ResultLength);
+  return value;
+}
+
+#endif
--- Includes/settingsMenu.hpp
+++ Includes/settingsMenu.hpp
@@ -2,6 +2,29 @@
 #define __SETTINGSMENU_H
 
 #include "menu.hpp"
+#include <vector>
+#include <string>
+
+class switchingMenuItem : public MenuItem {
+protected:
+  std::vector<std::string> values;
+  std::string valuedLabel;
+  int selected;
+public:
+  switchingMenuItem(std::string const& label, std::vector<std::string>);
+  std::string_view getLabel() const;
+};
+
+class togglingEEPROMMenuItem : public MenuItem {
+  bool value = false;
+  std::string valuedLabel;
+  uint32_t bitmask;
+  uint32_t valueIndex;
+public:
+  togglingEEPROMMenuItem(std::string const& label, uint32_t vI, uint32_t bmask);
+  std::string_view getLabel() const;
+  void execute(Menu *);
+};
 
 class settingsMenu : public MenuNode {
 public:
