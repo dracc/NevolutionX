64f6fb470a174655e82ab54f52b8bd056cba5009
Erik Abair <erik.abair@gmail.com>
2021-09-16 19:24:12 -0700

    settings: Makes f & g mounting configurable.

--- Includes/subsystems.h
+++ Includes/subsystems.h
@@ -1,10 +1,12 @@
 #ifndef SUBSYSTEMS_H
 #define SUBSYSTEMS_H
 
+#include "config.hpp"
+
 #ifdef NXDK
 void mountHomeDir(const char Letter);
 #endif
-int init_systems();
+int init_systems(const Config &config);
 void shutdown_systems(int systems);
 
 #endif
