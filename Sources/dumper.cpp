c5db53a6a03412d142420d7dde446e79939ba4bf
Lucas Jansson <dracc@dracc.se>
2022-01-05 13:39:48 +0100

    dump: Experiments

--- Sources/dumper.cpp
+++ Sources/dumper.cpp
@@ -4,26 +4,29 @@
 #include "infoLog.hpp"
 #include "menu.hpp"
 
-#define BIOSAREA ((void*)0xFFFC0000)
+#define BIOSAREA ((void*)0xFFF00000)
 
 Dumper::Dumper(std::string const& label) : MenuItem(label) {
 }
 
 void Dumper::execute(Menu*) {
-  WIN32_FIND_DATAA fData;
-  HANDLE fHandle = FindFirstFileA("C:\\Backup", &fData);
-  if (fHandle == INVALID_HANDLE_VALUE) {
-    CreateDirectory("C:\\Backup", nullptr);
-    fHandle = FindFirstFileA("C:\\Backup", &fData);
+  std::string folder = "C:\\Backup";
+  HANDLE backupFolder = CreateFileA(folder.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING,
+                                    FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS,
+                                    NULL);
+  if (backupFolder == INVALID_HANDLE_VALUE) {
+    CreateDirectory(folder.c_str(), nullptr);
+    backupFolder = CreateFileA(folder.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING,
+                               FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS,
+                               NULL);
   }
-  if (fData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-    std::ofstream BiosDump("C:\\Backup\\bios.bin", std::ofstream::binary);
-    BiosDump.write(static_cast<char*>(BIOSAREA), 256 * 1024);
-    BiosDump.flush();
-    BiosDump.close();
-    InfoLog::outputLine(InfoLog::INFO, "Dumped BIOS to C:\\Backup\\bios.bin");
-  } else {
+  if (backupFolder == INVALID_HANDLE_VALUE) {
     InfoLog::outputLine(InfoLog::WARNING, "C:\\Backup folder not found nor created!");
+  } else {
+    std::ofstream BiosDump("C:\\Backup\\bios.bin",
+                           std::ofstream::binary | std::ofstream::trunc);
+    BiosDump.write(static_cast<char*>(BIOSAREA), 256 * 1024);
+    InfoLog::outputLine(InfoLog::INFO, "Dumped BIOS to C:\\Backup\\bios.bin");
   }
-  FindClose(fHandle);
+  FindClose(backupFolder);
 }
