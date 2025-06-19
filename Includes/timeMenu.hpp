8d374c429d8302fa27948087cae395bf5cbe9132
Lucas <dracc@dracc.se>
2025-06-19 11:27:35 +0200

    clang-format: Update timeMenu.hpp

--- Includes/timeMenu.hpp
+++ Includes/timeMenu.hpp
@@ -23,9 +23,8 @@ public:
          uint32_t dststart,
          int32_t stdbias,
          int32_t dstbias) :
-      name(name),
-      bias(bias), stdname(stdname), dstname(dstname), stdstart(stdstart),
-      dststart(dststart), stdbias(stdbias), dstbias(dstbias){};
+      name(name), bias(bias), stdname(stdname), dstname(dstname), stdstart(stdstart),
+      dststart(dststart), stdbias(stdbias), dstbias(dstbias) {};
 };
 
 class TimeItem : public MenuItem {
